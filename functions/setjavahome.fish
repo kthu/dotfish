ef01962f3e8a1218374d0a8b6d3dd1fce2b4329b
Kjetil Thuen <Kjetil.Thuen@nsd.uib.no>
2012-10-09 09:39:41 +0200

    Added function for setting JAVA_HOME
    This script will try to figure out the best bet for where java lives and
    set $JAVA_HOME accordingly for it self and all children.

---
+++ functions/setjavahome.fish
@@ -0,0 +1,15 @@
+function setjavahome -d "Sets JAVA_HOME for current shell and children"
+   if not test -d "$JAVA_HOME"
+      if test -x (which readlink)
+         echo "\$JAVA_HOME not set. Will try to guess"
+         set JAVAC_EXE (which javac)
+         set JAVAC_PLACEMENT (readlink -f "$JAVAC_EXE")
+         set JAVA_HOME (dirname "$JAVAC_PLACEMENT")
+         set --global --export JAVA_HOME (dirname "$JAVA_HOME")
+         echo "Will try to use $JAVA_HOME as \$JAVA_HOME"
+      else
+         echo $JAVA_HOME is not set!
+         exit 1
+      end
+   end
+end
