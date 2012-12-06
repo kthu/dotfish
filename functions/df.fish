51f76940b9970ed0748b26da280dea88cfb4c82c
Kjetil Thuen <Kjetil.Thuen@nsd.uib.no>
2012-12-06 15:11:20 +0100

    Add support for discus. Add default options to dfc

--- functions/df.fish
+++ functions/df.fish
@@ -2,12 +2,16 @@ function df -d "Display available diskspace" --no-scope-shadowing
    set -l PATH $PATH /usr/local/bin ~/bin
 
    if test -e (which dfc)
-     eval (which dfc) $argv
+     eval (which dfc) -T -t -tmpfs,devtmpfs,ecryptfs $argv
    else 
-      if test -e (which df)
-         eval (which df) $argv
+      if test -e (which discus)
+        eval (which discus) $argv
       else
-        echo "df executable not found"
+         if test -e (which df)
+            eval (which df) $argv
+         else
+           echo "df executable not found"
+         end
       end
    end
 end
