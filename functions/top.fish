fe7616522d826dc535f2462ceee6780763a25d02
Kjetil Thuen <Kjetil.Thuen@nsd.uib.no>
2012-12-06 13:03:33 +0100

    Fixed typo

--- functions/top.fish
+++ functions/top.fish
@@ -1,12 +1,13 @@
 function top -d "Display processes"
    set -l PATH $PATH /usr/local/bin ~/bin
+
    if test -e (which htop)
-     eval (which htop)  $argv
+     eval (which htop) $argv
    else 
       if test -e (which top)
         eval (which top) $argv
       else
         echo "totopxecutable not found"
       end
-	top
+   end
 end
