7a505dd13f9d2f6a5b8be225267b5bb867d06cd2
Kjetil Thuen <Kjetil.Thuen@nsd.uib.no>
2013-02-28 09:10:07 +0100

    Whitespace...

--- functions/mvn.fish
+++ functions/mvn.fish
@@ -1,7 +1,7 @@
 function mvn -d "Like mvn, but with color"
    set -l PATH $PATH /usr/local/bin ~/bin
    if test -e (which mvnc)
-     eval (which mvnc)  $argv
+     eval (which mvnc) $argv
    else 
       if test -e (which mvn)
         eval (which mvn) $argv
