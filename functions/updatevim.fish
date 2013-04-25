fae80511c7f8659acb01169c75fa5e570003154d
Kjetil Thuen <kjetil.thuen@nsd.uib.no>
2013-04-25 09:54:34 +0200

    Change back to current dir after updating vim

--- functions/updatevim.fish
+++ functions/updatevim.fish
@@ -1,6 +1,8 @@
 function updatevim -d "Download new vundles, clean up the old ones"
-    set SHELL (which sh)
+    set CURRENTDIR (pwd)
     cd ~/.vim/
+    set SHELL (which sh)
     vim +BundleInstall! +BundleClean +qall
     set SHELL (which fish)
+    cd $CURRENTDIR
 end
