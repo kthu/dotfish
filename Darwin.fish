5e6537413891d8ca48e638e7c6b226af571a11d1
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2013-06-08 13:35:41 +0200

    Found out about Homebrew\'s --override-system-vim option. Much cleaner

--- Darwin.fish
+++ Darwin.fish
@@ -3,25 +3,6 @@
 # - Your user specific file (~/.config/fish/<username>.fish)
 # - Your host specific file (~/.config/fish/<hostname>.fish)
 
-# Textmate defaults to /usr/bin/mate, but let's check for /usr/local/bin/mate
-if test -f "/usr/local/bin/mate"
-   set -x EDITOR "/usr/local/bin/mate -w"
-else
-   if test -f "/usr/bin/mate"
-      set -x EDITOR "/usr/bin/mate -w"
-   end
-end
-
-#Use MacVim in terminal
-if test -f "/usr/local/bin/mvim"
-   function vim -d "MacVim in terminal" 
-      mvim -v $argv 
-   end
-   function vi -d "MacVim in terminal" 
-      mvim -v $argv
-   end
-end
-
 #Brew wants to be first in path
 set PATH /usr/local/bin $PATH
 
