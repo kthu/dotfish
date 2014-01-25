0c9b603704cd064b0578e17c6c81768072826bfd
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2014-01-25 16:19:11 +0100

    Tell Perl to use utf8 before creating entities

--- functions/utf2xml.fish
+++ functions/utf2xml.fish
@@ -1,3 +1,4 @@
-function utf2xml -d "Convert utf text to ascii with html entities"
-         perl -MHTML::Entities -ne 'print encode_entities($_)' $argv
+function utf2xml -d "Convert utf text from stdin to ascii with html entities"
+   set PERL_UNICODE S
+   perl -MHTML::Entities -Mutf8 -ne 'print encode_entities($_)'
 end
