c1c38ffd1873ddaac5592d264f4ef887d21df4eb
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2017-03-12 11:32:17 +0100

    Updated thefuck wrapper

--- functions/df.fish
+++ functions/df.fish
@@ -1,13 +1,13 @@
 function df -d "Display available diskspace" --no-scope-shadowing
   set command_alternatives dfc discus df
   # Command specific parameters
-  set dfc "-T -t -tmpfs,devtmpfs,ecryptfs"
+  set dfc "-T -t -aufs,cgmfs,tmpfs,devtmpfs,ecryptfs -w"
 
   for i in (seq (count $command_alternatives))
     set alternative $command_alternatives[$i]
     if command_exists $alternative
       set args $$command_alternatives[$i]
-      eval (which $alternative) $args $argv
+      eval (which $alternative) $args $argv 2>/dev/null
       break
     end
   end
