e447ba61bcf07431f5231de8058dfeb9bf532ae0
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2018-08-07 16:26:58 +0200

    Dont source profile.d files that do not exist

--- Linux.fish
+++ Linux.fish
@@ -1,14 +1,17 @@
 # Functions specific to Linux users
 # Please note that this file is version controlled.
 # Use one of the local files (hostname or username) for user/host specific settings
-bass source /etc/profile.d/apps-bin-path.sh
 
-bass source /etc/profile.d/cedilla-portuguese.sh
+function source_profile_file
+   if test -f /etc/profile.d/$argv.sh
+      bass source /etc/profile.d/$argv.sh
+   end
+end
 
-bass source /etc/profile.d/vte-2.91.sh
+source_profile_file apps-bin-path
+source_profile_file cedilla-portuguese
+source_profile_file vte-2.91 
+source_profile_file bash_completion
+source_profile_file input-method-config
+source_profile_file xdfg_dirs_desktop_session
 
-bass source /etc/profile.d/bash_completion.sh
-
-bass source /etc/profile.d/input-method-config.sh
-
-bass source /etc/profile.d/xdg_dirs_desktop_session.sh
