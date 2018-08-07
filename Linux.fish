4c2b648e0627ac24d9e669bebf1dccebbf7f178f
Kjetil Thuen <kjetil.thuen@nsd.no>
2018-08-07 16:09:32 +0200

    Add fisher and bass, source /etc/profile.d on Linux

--- Linux.fish
+++ Linux.fish
@@ -1,3 +1,14 @@
 # Functions specific to Linux users
-# Please note that this file is version controlled. 
+# Please note that this file is version controlled.
 # Use one of the local files (hostname or username) for user/host specific settings
+bass source /etc/profile.d/apps-bin-path.sh
+
+bass source /etc/profile.d/cedilla-portuguese.sh
+
+bass source /etc/profile.d/vte-2.91.sh
+
+bass source /etc/profile.d/bash_completion.sh
+
+bass source /etc/profile.d/input-method-config.sh
+
+bass source /etc/profile.d/xdg_dirs_desktop_session.sh
---
+++ functions/__bass.py
@@ -0,0 +1,1 @@
+/home/kjetil/.config/fisherman/bass/functions/__bass.py
