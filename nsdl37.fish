520039d3732bbee0e454c3027a17d197b4161b20
Kjetil Thuen <kjetil.thuen@nsd.no>
2018-08-20 11:44:09 +0200

    Commit host and user configs

---
+++ nsdl37.fish
@@ -0,0 +1,10 @@
+function tfl -d "Connect to Transport for London"
+  chromium-browser --temp-profile --proxy-server=socks5://localhost:9998 http://10.234.225.107:8080 &
+  ssh tfl
+end
+
+. ~/.config/fish/solarized-colors.fish
+
+if not pgrep -U (id -u) gpg-agent > /dev/null
+  gpg-connect-agent /bye >/dev/null 2>&1
+end
