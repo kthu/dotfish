8c798595452cf9db3e67bde247910193ece2c60f
Kjetil Thuen <kjetil.thuen@nsd.no>
2018-08-20 11:21:34 +0200

    Remove commented code

--- config.fish
+++ config.fish
@@ -8,64 +8,6 @@ else
    # Use subdir for fisherman
    set fish_path ~/.config/fish/fisherman
 
-#   function parse_git_branch
-#      sh -c 'git branch --no-color 2> /dev/null' | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
-#   end
-
-#   function parse_svn_tag_or_branch
-#           sh -c 'svn info | grep "^URL:" | egrep -o "(tags|branches)/[^/]+|trunk" | egrep -o "[^/]+$"'
-#   end
-#
-#   function parse_svn_revision
-#      sh -c 'svn info 2> /dev/null' | sed -n '/^Revision/p' | sed -e 's/^Revision: \(.*\)/\1/'
-#   end
-
-#   function parse_git_status
-#      git diff --quiet HEAD ^&-
-#      if test $status = 1
-#         printf ' %s≠%s%s' (set_color red) (git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/*\(.*\)/\1/') (set_color normal)
-#      else
-#         printf '%s%s%s' (set_color blue) (git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/*\(.*\)/\1/') (set_color normal)
-#      end
-#   end
-#
-#   # Better (imho) Git prompt than the default from fish nuggets
-#   function fish_prompt -d "Write out the prompt"
-#      # User
-#      printf '%s%s%s ' (set_color yellow)(whoami)(set_color white)
-#
-#      # Host (if remote)
-#      set --query SSH_CLIENT; and printf '%s %s %s' at (set_color red)(hostname|cut -d . -f 1)(set_color white)
-#
-#      # Path
-#      printf 'in '
-#      # Color writeable dirs green, read-only dirs red
-#      if test -w "."
-#         printf '%s%s' (set_color green) (prompt_pwd)
-#      else
-#         printf '%s%s' (set_color red) (prompt_pwd)
-#      end
-#
-#      if test -d ".svn"
-#         # Print subversion tag or branch
-#         printf ' %s%s%s' (set_color normal) (set_color blue) (parse_svn_tag_or_branch)
-#         # Print subversion revision
-#         printf '%s%s@%s' (set_color normal) (set_color blue) (parse_svn_revision)
-#      end
-#
-#      # Print git branch
-#      printf '%s%s' (set_color normal) (parse_git_status)
-#      printf '%s> ' (set_color -o white)
-#
-#      #Put dir in z history
-#      z --add (pwd)
-#
-#      #Tell vre about dir so that it can open new tabs here
-#      if test "$COLORTERM" = "gnome-terminal"
-#        printf "\033]7;file://%s%s\a" (hostname) (pwd)
-#      end
-#   end
-
    # Load custom settings for current hostname
    set HOST_SPECIFIC_FILE ~/.config/fish/(hostname).fish
    if test -f $HOST_SPECIFIC_FILE
