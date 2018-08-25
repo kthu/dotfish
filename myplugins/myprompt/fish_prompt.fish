0bed33bd3d7e902594d42dad6d5c2acc87d1f9cc
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2018-08-25 21:37:12 +0200

    Improved padding behaviour after starting indicators

--- myplugins/myprompt/fish_prompt.fish
+++ myplugins/myprompt/fish_prompt.fish
@@ -21,7 +21,12 @@ function show_status -d "Function to show the current status"
   if [ $RETVAL -ne 0 ]
     prompt_segment red white " ▲ "
     set pad ""
+    if [ -n "$SSH_CLIENT" ]
+      #Oh nothing
+    else
+      prompt_segment normal normal " "
     end
+  end
   if [ -n "$SSH_CLIENT" ]
       prompt_segment blue white " SSH: "
       prompt_segment normal normal " "
