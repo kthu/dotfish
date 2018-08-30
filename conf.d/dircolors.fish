ba9bcb2ff40d0c687c5cda1ba0e330565005bd2e
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2018-08-30 23:26:31 +0200

    Set solarized theme when using bat

--- conf.d/dircolors.fish
+++ conf.d/dircolors.fish
@@ -1,4 +1,16 @@
 # Set dircolors if ~/.dircolors exists
+
+set command_alternatives dircolors gdircolors
+
 if test -e ~/.dircolors
-     set -x DIR_COLORS (dircolors -c | cut -d' '  -f 3-)
+
+     for i in (seq (count $command_alternatives))
+          set alternative $command_alternatives[$i]
+          if command_exists $alternative
+               set -x DIR_COLORS (eval (which $alternative) -c | cut -d' '  -f 3-)
+               #eval (which $alternative) $args $argv 2>/dev/null
+               break
+          end
+     end
 end
+     # set -x DIR_COLORS (dircolors -c | cut -d' '  -f 3-)
