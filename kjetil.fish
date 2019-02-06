7feb14ed99d30309e7c809705a0f30ee89447099
Kjetil Thuen <kjetil.thuen@nsd.no>
2019-02-06 09:05:36 +0100

    alias instead of abbr for dotstore

--- kjetil.fish
+++ kjetil.fish
@@ -8,10 +8,10 @@ function cdl; cd $_ ; end
 function forallf; find . -type f -print0 | xargs -0 ; end
 function foralld; find . -type d -print0 | xargs -0 ; end
 function g; gvim --remote-silent $argv ; end
+function dotstore; /usr/bin/git --git-dir=$HOME/.local/var/dotstore/ --work-tree=$HOME $argv ; end
 
 abbr exot exit
 abbr cls reset
 abbr p pwd
 abbr c cat
-abbr dotstore "/usr/bin/git --git-dir=$HOME/.local/var/dotstore/ --work-tree=$HOME"
 
