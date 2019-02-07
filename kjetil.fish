1b9d879b3e2286819b75c2e3ef5759c21c8884ff
Kjetil Thuen <kjetil.thuen@nsd.no>
2019-02-07 09:55:27 +0100

    Colors in man pages

--- kjetil.fish
+++ kjetil.fish
@@ -1,7 +1,14 @@
 set EDITOR vim
 set VISUAL vim
-set PAGER vimpager
+set PAGER less
 set -x MAVEN_COLOR true
+
+set -xU LESS_TERMCAP_md (printf "\e[01;31m")
+set -xU LESS_TERMCAP_me (printf "\e[0m")
+set -xU LESS_TERMCAP_se (printf "\e[0m")
+set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -xU LESS_TERMCAP_ue (printf "\e[0m")
+set -xU LESS_TERMCAP_us (printf "\e[01;32m")
 
 #Aliaser
 function cdl; cd $_ ; end
