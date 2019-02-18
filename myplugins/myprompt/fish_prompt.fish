e27a11a55b8d541efe42c2c7a8764112e86f7480
Kjetil Thuen <kjetil.thuen@nsd.no>
2019-02-18 11:21:19 +0100

    Slightly modified agnoster prompt (vi mode on left)

--- myplugins/myprompt/fish_prompt.fish
+++ myplugins/myprompt/fish_prompt.fish
@@ -1,99 +1,280 @@
-set -g pad " "
+# name: Agnoster
+# agnoster's Theme - https://gist.github.com/3712874
+# A Powerline-inspired theme for FISH
+#
+# # README
+#
+# In order for this theme to render correctly, you will need a
+# [Powerline-patched font](https://gist.github.com/1595572).
 
-## Function to show a segment
-function prompt_segment -d "Function to show a segment"
-  # Get colors
-  set -l bg $argv[1]
-  set -l fg $argv[2]
+## Set this options in your config.fish (if you want to :])
+# set -g theme_display_user yes
+# set -g theme_hide_hostname yes
+# set -g theme_hide_hostname no
+# set -g default_user your_normal_user
 
-  # Set 'em
-  set_color -b $bg
-  set_color $fg
 
-  # Print text
+
+set -g current_bg NONE
+set segment_separator \uE0B0
+set right_segment_separator \uE0B0
+
+# ===========================
+# Color setting
+
+# You can set these variables in config.fish like:
+# set -g color_dir_bg red
+# If not set, default color from agnoster will be used.
+# ===========================
+
+set -q color_virtual_env_bg; or set color_virtual_env_bg white
+set -q color_virtual_env_str; or set color_virtual_env_str black
+set -q color_user_bg; or set color_user_bg black
+set -q color_user_str; or set color_user_str yellow
+set -q color_dir_bg; or set color_dir_bg blue
+set -q color_dir_str; or set color_dir_str black
+set -q color_hg_removed_bg; or set color_hg_removed_bg red
+set -q color_hg_removed_str; or set color_hg_removed_str white
+set -q color_hg_untracked_bg; or set color_hg_untracked_bg yellow
+set -q color_hg_untracked_str; or set color_hg_untracked_str black
+set -q color_hg_bg; or set color_hg_bg green
+set -q color_hg_str; or set color_hg_str black
+set -q color_git_dirty_bg; or set color_git_dirty_bg yellow
+set -q color_git_dirty_str; or set color_git_dirty_str black
+set -q color_git_bg; or set color_git_bg green
+set -q color_git_str; or set color_git_str black
+set -q color_svn_bg; or set color_svn_bg green
+set -q color_svn_str; or set color_svn_str black
+set -q color_status_nonzero_bg; or set color_status_nonzero_bg black
+set -q color_status_nonzero_str; or set color_status_nonzero_str red
+set -q color_status_superuser_bg; or set color_status_superuser_bg black
+set -q color_status_superuser_str; or set color_status_superuser_str yellow
+set -q color_status_jobs_bg; or set color_status_jobs_bg black
+set -q color_status_jobs_str; or set color_status_jobs_str cyan
+
+
+# ===========================
+# Helper methods
+# ===========================
+
+set -g __fish_git_prompt_showdirtystate 'yes'
+set -g __fish_git_prompt_char_dirtystate '±'
+set -g __fish_git_prompt_char_cleanstate ''
+
+function parse_git_dirty
+  set -l submodule_syntax
+  set submodule_syntax "--ignore-submodules=dirty"
+  set git_dirty (command git status --porcelain $submodule_syntax  2> /dev/null)
+  if [ -n "$git_dirty" ]
+    if [ $__fish_git_prompt_showdirtystate = "yes" ]
+      echo -n "$__fish_git_prompt_char_dirtystate"
+    end
+  else
+    if [ $__fish_git_prompt_showdirtystate = "yes" ]
+      echo -n "$__fish_git_prompt_char_cleanstate"
+    end
+  end
+end
+
+
+# ===========================
+# Segments functions
+# ===========================
+
+function prompt_segment -d "Function to draw a segment"
+  set -l bg
+  set -l fg
+  if [ -n "$argv[1]" ]
+    set bg $argv[1]
+  else
+    set bg normal
+  end
+  if [ -n "$argv[2]" ]
+    set fg $argv[2]
+  else
+    set fg normal
+  end
+  if [ "$current_bg" != 'NONE' -a "$argv[1]" != "$current_bg" ]
+    set_color -b $bg
+    set_color $current_bg
+    echo -n "$segment_separator "
+    set_color -b $bg
+    set_color $fg
+  else
+    set_color -b $bg
+    set_color $fg
+    echo -n " "
+  end
+  set current_bg $argv[1]
   if [ -n "$argv[3]" ]
-    echo -n -s $argv[3]
+    echo -n -s $argv[3] " "
   end
 end
 
-## Function to show current status
-function show_status -d "Function to show the current status"
-  if [ $RETVAL -ne 0 ]
-    prompt_segment red white " ▲ "
-    set pad ""
-    if [ -n "$SSH_CLIENT" ]
-      #Oh nothing
+function prompt_finish -d "Close open segments"
+  if [ -n $current_bg ]
+    set_color -b normal
+    set_color $current_bg
+    echo -n "$segment_separator "
+  end
+  set -g current_bg NONE
+end
+
+
+# ===========================
+# Theme components
+# ===========================
+
+function prompt_virtual_env -d "Display Python virtual environment"
+  if test "$VIRTUAL_ENV"
+    prompt_segment $color_virtual_env_bg $color_virtual_env_str (basename $VIRTUAL_ENV)
+  end
+end
+
+function prompt_user -d "Display current user if different from $default_user"
+  if [ "$theme_display_user" = "yes" ]
+    if [ "$USER" != "$default_user" -o -n "$SSH_CLIENT" ]
+      set USER (whoami)
+      get_hostname
+      if [ $HOSTNAME_PROMPT ]
+        set USER_PROMPT $USER@$HOSTNAME_PROMPT
+      else
+        set USER_PROMPT $USER
+      end
+      prompt_segment $color_user_bg $color_user_str $USER_PROMPT
+    end
+  else
+    get_hostname
+    if [ $HOSTNAME_PROMPT ]
+      prompt_segment $color_user_bg $color_user_str $HOSTNAME_PROMPT
+    end
+  end
+end
+
+function get_hostname -d "Set current hostname to prompt variable $HOSTNAME_PROMPT if connected via SSH"
+  set -g HOSTNAME_PROMPT ""
+  if [ "$theme_hide_hostname" = "no" -o \( "$theme_hide_hostname" != "yes" -a -n "$SSH_CLIENT" \) ]
+    set -g HOSTNAME_PROMPT (hostname)
+  end
+end
+
+function prompt_dir -d "Display the current directory"
+  prompt_segment $color_dir_bg $color_dir_str (prompt_pwd)
+end
+
+
+function prompt_hg -d "Display mercurial state"
+  set -l branch
+  set -l state
+  if command hg id >/dev/null 2>&1
+    if command hg prompt >/dev/null 2>&1
+      set branch (command hg prompt "{branch}")
+      set state (command hg prompt "{status}")
+      set branch_symbol \uE0A0
+      if [ "$state" = "!" ]
+        prompt_segment $color_hg_removed_bg $color_hg_removed_str "$branch_symbol $branch ±"
+      else if [ "$state" = "?" ]
+          prompt_segment $color_hg_untracked_bg $color_hg_untracked_str "$branch_symbol $branch ±"
+        else
+          prompt_segment $color_hg_bg $color_hg_str "$branch_symbol $branch"
+      end
+    end
+  end
+end
+
+
+function prompt_git -d "Display the current git state"
+  set -l ref
+  set -l dirty
+  if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+    set dirty (parse_git_dirty)
+    set ref (command git symbolic-ref HEAD 2> /dev/null)
+    if [ $status -gt 0 ]
+      set -l branch (command git show-ref --head -s --abbrev |head -n1 2> /dev/null)
+      set ref "➦ $branch "
+    end
+    set branch_symbol \uE0A0
+    set -l branch (echo $ref | sed  "s-refs/heads/-$branch_symbol -")
+    if [ "$dirty" != "" ]
+      prompt_segment $color_git_dirty_bg $color_git_dirty_str "$branch $dirty"
     else
-      prompt_segment normal normal " "
-    end
-  end
-  if [ -n "$SSH_CLIENT" ]
-      prompt_segment blue white " SSH: "
-      prompt_segment normal normal " "
-      set pad ""
-    end
-end
-
-function show_virtualenv -d "Show active python virtual environments"
-  if set -q VIRTUAL_ENV
-    set -l venvname (basename "$VIRTUAL_ENV")
-    prompt_segment normal white " ($venvname) "
-  end
-end
-
-## Show user if not in default users
-function show_user -d "Show user"
-  if not contains $USER $default_user; or test -n "$SSH_CLIENT"
-    set -l host (hostname -s)
-    set -l who (whoami)
-    prompt_segment normal yellow "$who"
-
-    # Skip @ bit if hostname == username
-    if [ "$USER" != "$HOST" ]
-      prompt_segment normal white "@"
-      prompt_segment normal green "$host "
-      set pad ""
+      prompt_segment $color_git_bg $color_git_str "$branch $dirty"
     end
   end
 end
 
-function _set_venv_project --on-variable VIRTUAL_ENV
-    if test -e $VIRTUAL_ENV/.project
-        set -g VIRTUAL_ENV_PROJECT (cat $VIRTUAL_ENV/.project)
-    end
-end
 
-# Show directory
-function show_pwd -d "Show the current directory"
-  set -l pwd
-  if [ (string match -r '^'"$VIRTUAL_ENV_PROJECT" $PWD) ]
-    set pwd (string replace -r '^'"$VIRTUAL_ENV_PROJECT"'($|/)' '≫ $1' $PWD)
-  else
-    set pwd (prompt_pwd)
+function prompt_svn -d "Display the current svn state"
+  set -l ref
+  if command svn info >/dev/null 2>&1
+    set branch (svn_get_branch)
+    set branch_symbol \uE0A0
+    set revision (svn_get_revision)
+    prompt_segment $color_svn_bg $color_svn_str "$branch_symbol $branch:$revision"
   end
-  prompt_segment normal blue "$pad$pwd"
 end
 
-# Show prompt w/ privilege cue
-function show_prompt -d "Shows prompt with cue for current priv"
-  set -l uid (id -u $USER)
+function svn_get_branch -d "get the current branch name"
+  svn info 2> /dev/null | awk -F/ \
+      '/^URL:/ { \
+        for (i=0; i<=NF; i++) { \
+          if ($i == "branches" || $i == "tags" ) { \
+            print $(i+1); \
+            break;\
+          }; \
+          if ($i == "trunk") { print $i; break; } \
+        } \
+      }'
+end
+
+function svn_get_revision -d "get the current revision number"
+  svn info 2> /dev/null | sed -n 's/Revision:\ //p'
+end
+
+
+function prompt_status -d "the symbols for a non zero exit status, root and background jobs"
+    if [ $RETVAL -ne 0 ]
+      prompt_segment $color_status_nonzero_bg $color_status_nonzero_str "✘"
+    end
+
+    # if superuser (uid == 0)
+    set -l uid (id -u $USER)
     if [ $uid -eq 0 ]
-    prompt_segment red white " ! "
-    set_color normal
-    echo -n -s " "
-  else
-    prompt_segment normal white "> "
+      prompt_segment $color_status_superuser_bg $color_status_superuser_str "⚡"
     end
 
-  set_color normal
+    # Jobs display
+    if [ (jobs -l | wc -l) -gt 0 ]
+      prompt_segment $color_status_jobs_bg $color_status_jobs_str "⚙"
+    end
 end
 
-## SHOW PROMPT
+function prompt_vi_mode -d 'vi mode status indicator'
+  switch $fish_bind_mode
+    case default
+      prompt_segment green black "N"
+    case insert
+      prompt_segment blue black "I"
+    case visual
+      prompt_segment purple black "V"
+  end
+end
+
+# ===========================
+# Apply theme
+# ===========================
+
 function fish_prompt
   set -g RETVAL $status
-  show_status
-  show_virtualenv
-  show_user
-  show_pwd
-  show_prompt
+  prompt_status
+  prompt_virtual_env
+  prompt_user
+  prompt_dir
+  type -q hg;  and prompt_hg
+  type -q git; and prompt_git
+  type -q svn; and prompt_svn
+  if test "$fish_key_bindings" = "fish_vi_key_bindings"
+    prompt_vi_mode
+  end
+  prompt_finish
 end
