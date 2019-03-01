4702b5e0cb4fe3fe80df7eb0e097bb77e6d19ad8
Kjetil Thuen <kjetil.thuen@nsd.no>
2019-03-01 13:13:23 +0100

    Fighting the bass

--- config.fish
+++ config.fish
@@ -4,27 +4,27 @@ if [ $status = 0 -a "$SSH_CLIENT" -a $TERM != "dumb" -a $TERM != "screen" -a $TE
 else
    set fish_greeting ""
    set -x CLICOLOR 1
-
    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
 
-   # Use subdir for fisherman
-   mkdir $XDG_CONFIG_HOME/fish/fisherman
-   set -U fish_path $XDG_CONFIG_HOME/fish/fisherman
+   # Set up fisherman paths
+   mkdir -p $XDG_CONFIG_HOME/fish/fisherman
 
+   set -U fisher_path $XDG_CONFIG_HOME/fish/fisherman
+   set fish_function_path $fish_function_path[1] $fisher_path/functions   $fish_function_path[2..-1]
+   set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+
+   for file in $fisher_path/conf.d/*.fish
+       builtin source $file 2> /dev/null
+   end
+
+   # Install fisherman
    if not functions -q fisher
      curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
      fish -c fisher
    end
 
-   set fish_function_path $fish_path/functions $fish_function_path
-   set fish_complete_path $fish_path/completions $fish_complete_path
-
-   for file in $fish_path/conf.d/*.fish
-     builtin source $file 2> /dev/null
-   end
-
    # Load custom settings for current hostname
-   set HOST_SPECIFIC_FILE ~/.config/fish/(hostname).fish
+   set HOST_SPECIFIC_FILE $XDG_CONFIG_HOME/fish/(hostname).fish
    if test -f $HOST_SPECIFIC_FILE
       . $HOST_SPECIFIC_FILE
    else
@@ -33,7 +33,7 @@ else
    end
 
    # Load custom settings for current user
-   set USER_SPECIFIC_FILE ~/.config/fish/(whoami).fish
+   set USER_SPECIFIC_FILE $XDG_CONFIG_HOME/fish/(whoami).fish
    if test -f $USER_SPECIFIC_FILE
       . $USER_SPECIFIC_FILE
    else
@@ -42,7 +42,7 @@ else
    end
 
    # Load custom settings for current OS
-   set PLATFORM_SPECIFIC_FILE ~/.config/fish/(uname -s).fish
+   set PLATFORM_SPECIFIC_FILE $XDG_CONFIG_HOME/fish/(uname -s).fish
    if test -f $PLATFORM_SPECIFIC_FILE
       . $PLATFORM_SPECIFIC_FILE
    else
