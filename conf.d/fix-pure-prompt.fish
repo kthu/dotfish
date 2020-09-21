83f468281cc99bc871b810af282b6d99722761ba
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2020-09-21 13:37:41 +0200

    Fix git symbols in pure prompt

--- conf.d/fix-pure-prompt.fish
+++ conf.d/fix-pure-prompt.fish
@@ -1,2 +1,5 @@
 set -U pure_symbol_prompt "❯"
 set -U pure_symbol_reverse_prompt "❮"
+set -U pure_symbol_git_unpulled_commits "⇣"
+set -U pure_symbol_git_unpushed_commits "⇡"
+set -U pure_symbol_git_dirty "*"
