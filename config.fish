1a224132293fe13c237c71f48cb6b78504ec2d7d
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2020-12-27 22:29:41 +0100

    Colored man pages

--- config.fish
+++ config.fish
@@ -27,7 +27,7 @@ if status is-interactive
       # Load custom settings for current hostname
       set HOST_SPECIFIC_FILE $XDG_CONFIG_HOME/fish/(hostname).fish
       if test -f $HOST_SPECIFIC_FILE
-         . $HOST_SPECIFIC_FILE
+         source $HOST_SPECIFIC_FILE
       else
          echo Creating host specific file: $HOST_SPECIFIC_FILE
          touch $HOST_SPECIFIC_FILE
@@ -36,7 +36,7 @@ if status is-interactive
       # Load custom settings for current user
       set USER_SPECIFIC_FILE $XDG_CONFIG_HOME/fish/(whoami).fish
       if test -f $USER_SPECIFIC_FILE
-         . $USER_SPECIFIC_FILE
+         source $USER_SPECIFIC_FILE
       else
          echo Creating user specific file: $USER_SPECIFIC_FILE
          touch $USER_SPECIFIC_FILE
@@ -45,7 +45,7 @@ if status is-interactive
       # Load custom settings for current OS
       set PLATFORM_SPECIFIC_FILE $XDG_CONFIG_HOME/fish/(uname -s).fish
       if test -f $PLATFORM_SPECIFIC_FILE
-         . $PLATFORM_SPECIFIC_FILE
+         source $PLATFORM_SPECIFIC_FILE
       else
          echo Creating platform specific file: $PLATFORM_SPECIFIC_FILE
          touch $PLATFORM_SPECIFIC_FILE
