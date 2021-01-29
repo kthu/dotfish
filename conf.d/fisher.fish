f58ab88c48587df9032a65534594deb9ca45fa3c
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2021-01-29 20:09:24 +0100

    Tidy up

--- conf.d/fisher.fish
+++ conf.d/fisher.fish
@@ -2,13 +2,8 @@
 
 set -U fisher_path $__fish_config_dir/fisherd
 
-#mkdir -p $fisher_path/functions
-#mkdir -p $fisher_path/completions
-#mkdir -p $fisher_path/conf.d
-
 contains $fisher_path/completions $fish_complete_path; or set -a fish_complete_path $fisher_path/completions
 contains $fisher_path/functions $fish_function_path; or set -a fish_function_path $fisher_path/functions
-
 
 for file in $fisher_path/conf.d/*.fish
   builtin source $file 2> /dev/null
