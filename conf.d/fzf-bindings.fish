5d84399182cfee475f8e54ee4cc7ba88830a1b54
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2021-01-29 13:53:17 +0100

    Use fd as fzf backend

--- conf.d/fzf-bindings.fish
+++ conf.d/fzf-bindings.fish
@@ -1,3 +1,5 @@
 if command_exists fzf
+    set -g FZF_DEFAULT_COMMAND "command fd --type f --hidden --follow --exclude .git \$dir"
+    set -g FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
     fzf_key_bindings
 end
