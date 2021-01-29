b4bdedb0d326732e6e7edadb036b6faae0a767d0
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2021-01-29 18:06:18 +0100

    Dont grow universal vars!!

--- conf.d/fisher.fish
+++ conf.d/fisher.fish
@@ -1,13 +1,13 @@
 # Set up custom fisher paths
 
-set -U fisher_path $XDG_CONFIG_HOME/fish/fisherd
+set -U fisher_path $__fish_config_dir/fisherd
 
 mkdir -p $fisher_path/functions
 mkdir -p $fisher_path/completions
 mkdir -p $fisher_path/conf.d
 
-set -U fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
-set -U fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+set -x fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set -x fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
 
 # Install fisher if not installed
 if not functions -q fisher
