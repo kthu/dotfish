e021517645446bb17f3d99ca15097b96b8c793f6
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2021-01-29 17:18:20 +0100

    Make sure paths with fisher dirs are global

--- config.fish
+++ config.fish
@@ -7,23 +7,6 @@ if status is-interactive
       set -x CLICOLOR 1
       set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
 
-      # Set up fisher paths
-      mkdir -p $XDG_CONFIG_HOME/fish/fisherd
-
-      set -U fisher_path $XDG_CONFIG_HOME/fish/fisherd
-      set fish_function_path $fish_function_path[1] $fisher_path/functions   $fish_function_path[2..-1]
-      set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
-
-      for file in $fisher_path/conf.d/*.fish
-          builtin source $file 2> /dev/null
-      end
-
-      # Install fisher
-      if not functions -q fisher
-        curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-        fisher update
-      end
-
       # Load custom settings for current hostname
       set HOST_SPECIFIC_FILE $XDG_CONFIG_HOME/fish/(hostname).fish
       if test -f $HOST_SPECIFIC_FILE
@@ -51,6 +34,29 @@ if status is-interactive
          touch $PLATFORM_SPECIFIC_FILE
       end
 
+      # Set up custom fisher paths
+      mkdir -p $XDG_CONFIG_HOME/fish/fisherd
+      
+      set -U fisher_path $XDG_CONFIG_HOME/fish/fisherd
+      mkdir -p $fisher_path/functions
+      mkdir -p $fisher_path/completions
+      mkdir -p $fisher_path/conf.d
+
+      set -U fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+      set -U fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+
+      # Install fisher
+      if not functions -q fisher
+        curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+        fisher update
+      end
+
+      for file in $fisher_path/conf.d/*.fish
+          source $file 2> /dev/null
+      end
+
+      # Prompt
       starship init fish | source
+
    end
 end
