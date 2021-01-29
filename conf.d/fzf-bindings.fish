73c2fd29ae507f4c4c7a5793e1adbce2052b2af4
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2021-01-29 14:01:11 +0100

    Improve fzf bindings

--- conf.d/fzf-bindings.fish
+++ conf.d/fzf-bindings.fish
@@ -1,5 +1,7 @@
-if command_exists fzf
-    set -g FZF_DEFAULT_COMMAND "command fd --type f --hidden --follow --exclude .git \$dir"
-    set -g FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+if command_exists fzf   
+    if command_exists fd
+        set -g FZF_DEFAULT_COMMAND "command fd --hidden --follow --exclude .git --search-path \$dir"
+        set -g FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+    end
     fzf_key_bindings
 end
