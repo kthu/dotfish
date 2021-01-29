b26fd84abca7458b94addb16a89efb3b703b602d
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2021-01-29 19:34:25 +0100

    Dont attempt installinf fisher. Will loop!

--- conf.d/fisher.fish
+++ conf.d/fisher.fish
@@ -2,20 +2,21 @@
 
 set -U fisher_path $__fish_config_dir/fisherd
 
-mkdir -p $fisher_path/functions
-mkdir -p $fisher_path/completions
-mkdir -p $fisher_path/conf.d
+#mkdir -p $fisher_path/functions
+#mkdir -p $fisher_path/completions
+#mkdir -p $fisher_path/conf.d
 
 contains $fisher_path/completions $fish_complete_path; or set -a fish_complete_path $fisher_path/completions
 contains $fisher_path/functions $fish_function_path; or set -a fish_function_path $fisher_path/functions
 
-# Install fisher if not installed
-if not functions -q fisher
-  curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-  fisher update
-end
 
 for file in $fisher_path/conf.d/*.fish
-    builtin source $file 2> /dev/null
+  builtin source $file 2> /dev/null
 end
 
+# Warn user to install fisher
+if not functions -q fisher
+  echo "Please install fisher:"
+  echo "  curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
+  echo "  fisher update"
+end      
