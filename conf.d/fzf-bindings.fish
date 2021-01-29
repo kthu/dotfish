9b1c07f82733ffc0428ea6dd35954328c4f875e9
Kjetil Thuen <Kjetil.Thuen@gmail.com>
2021-01-29 14:09:15 +0100

    Use fisher plugin for fzf config

--- conf.d/fzf-bindings.fish
+++
@@ -1,7 +0,0 @@
-if command_exists fzf   
-    if command_exists fd
-        set -g FZF_DEFAULT_COMMAND "command fd --hidden --follow --exclude .git --search-path \$dir"
-        set -g FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-    end
-    fzf_key_bindings
-end
